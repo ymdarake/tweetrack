0cd84f1a18ee0412556ae152f705e136530898fb
Takumi.Sakamoto <t.sakamoto.ouen@gmail.com>
2018-06-26 03:17:54 +0900

    [fix]データファイルをignore

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 
-var server = app.listen(process.env.PORT, function() {
+var server = app.listen(process.env.PORT || 8080, function() {
     console.log("Node.js is listening to PORT:" + server.address().port);
 });
 
