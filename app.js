127fb407fed2b0169b134ff6e206e276da8eb9c7
Takumi.Sakamoto <t.sakamoto.ouen@gmail.com>
2018-06-26 02:44:52 +0900

    [fix]dynamic port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 
-var server = app.listen(process.env.port, function() {
+var server = app.listen(process.env.PORT, function() {
     console.log("Node.js is listening to PORT:" + server.address().port);
 });
 
