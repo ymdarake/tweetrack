2036a38005c9c55d9731ac9963353570283faf99
Takumi.Sakamoto <t.sakamoto.ouen@gmail.com>
2018-06-26 02:32:38 +0900

    [add]express

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var app = express();
+
+var server = app.listen(80, function() {
+    console.log("Node.js is listening to PORT:" + server.address().port);
+});
+
+app.get("/", function(req, res, next){
+    res.sendFile(__dirname+"/index.html");
+});
+app.get("/rts.js", function(req, res, next){
+    res.sendFile(__dirname+"/rts.js");
+});
+app.get("/favorites.js", function(req, res, next){
+    res.sendFile(__dirname+"/favorites.js");
+});
+app.get("/update-data", function(req, res, next){
+    require(__dirname+"/update-data.js");
+    res.status(200).send('ok!');
+});
